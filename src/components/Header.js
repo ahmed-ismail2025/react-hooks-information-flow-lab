e77545fb88e22664093d4269ec7d3a445c93527e
ahmed-ismail2025 <ahmedismail9044@gmail.com>
2025-08-07 17:17:28 +0300

    Pass all tests for information flow lab

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header({ onDarkModeClick }) {
+  return (
+    <header>
+      <h1>Shopster</h1>
+      <button onClick={onDarkModeClick}>Dark Mode</button>
+    </header>
+  );
+}
+
+export default Header;
